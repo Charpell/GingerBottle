7d61b9c33205bd58928d005e08823e2161f02c86
Ebuka Umeh <charpellumeh@gmail.com>
2020-08-21 08:59:01 +0100

    users

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -23,8 +23,32 @@ async function feed(parent, args, context, info) {
   }
 }
 
+async function user(parent, args, context, info) {
+  const where = args.filter
+    ? {
+      OR: [
+        { name: { contains: args.filter } }
+      ],
+    }
+    : {}
+
+  const users = await context.prisma.user.findMany({
+    where,
+    skip: args.skip,
+    take: args.take
+  })
+
+  const count = await context.prisma.user.count({ where })
+
+  return {
+    users,
+    count,
+  }
+}
+
   
   module.exports = {
-    feed
+    feed,
+    user
   }
   
